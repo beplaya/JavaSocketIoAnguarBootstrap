fcef9678f41b8681729fdc6d09476aa0b0766659
Grant James <grant.james@cardinalhealth.com>
2016-05-27 10:04:51 -0400

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# JavaSocketIoAnguarBootstrap
+
+
+##### Bootstrap a simple Socket IO app with Java in the back and JS in the front with angular JSand bootstrap css.
---
+++ src/main/java/com/gjames/socketio/demo/ChatLauncher.java
@@ -0,0 +1,33 @@
+package com.gjames.socketio.demo;
+
+import com.corundumstudio.socketio.listener.*;
+import com.corundumstudio.socketio.*;
+
+public class ChatLauncher {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Configuration config = new Configuration();
+        SocketConfig sockConfig = new SocketConfig();
+        sockConfig.setReuseAddress(true);
+        config.setSocketConfig(sockConfig);
+        config.setHostname("localhost");
+        config.setPort(9092);
+
+        final SocketIOServer server = new SocketIOServer(config);
+        server.addEventListener("chatevent", ChatObject.class, new DataListener<ChatObject>() {
+            @Override
+            public void onData(SocketIOClient client, ChatObject data, AckRequest ackRequest) {
+                // broadcast messages to all clients
+                server.getBroadcastOperations().sendEvent("chatevent", data);
+            }
+        });
+
+        server.start();
+
+        Thread.sleep(Integer.MAX_VALUE);
+
+        server.stop();
+    }
+
+}
